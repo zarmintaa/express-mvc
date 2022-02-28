4c9c0c2d83247085f55ea1558f5ed383230ea555
ZeusCoder <zeusselalusalah@gmail.com>
2022-02-28 17:29:12 +0700

    create shop structure

--- controllers/product.js
+++ controllers/product.js
@@ -1,7 +1,7 @@
 const Product = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
-  res.render("add-product", {
+  res.render("admin/add-product", {
     pageTitle: "Add Product",
     path: "/admin/add-product",
     formsCSS: true,
@@ -11,7 +11,6 @@ exports.getAddProduct = (req, res, next) => {
 };
 
 exports.postAddProduct = (req, res, next) => {
-  // products.push({ title: req.body.title });
   const product = new Product(req.body.title);
   product.save();
   res.redirect("/");
@@ -19,7 +18,7 @@ exports.postAddProduct = (req, res, next) => {
 
 exports.getProducts = (req, res, next) => {
   const products = Product.fetchAll((products) => {
-    res.render("shop", {
+    res.render("shop/product-list", {
       prods: products,
       pageTitle: "Shop",
       path: "/",
