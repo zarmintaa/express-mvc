de29087a945127f3c9353f2b4ee826271283bf81
ZeusCoder <zeusselalusalah@gmail.com>
2022-03-02 09:32:50 +0700

    Applied changes

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,42 +1,42 @@
-const Product = require("../models/product");
+const Product = require('../models/product');
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll((products) => {
-    res.render("shop/product-list", {
+  Product.fetchAll(products => {
+    res.render('shop/product-list', {
       prods: products,
-      pageTitle: "Shop",
-      path: "/",
+      pageTitle: 'All Products',
+      path: '/products'
     });
   });
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll((products) => {
-    res.render("shop/index", {
+  Product.fetchAll(products => {
+    res.render('shop/index', {
       prods: products,
-      pageTitle: "Shop",
-      path: "/",
+      pageTitle: 'Shop',
+      path: '/'
     });
   });
 };
 
 exports.getCart = (req, res, next) => {
-  res.render("shop/cart", {
-    pageTitle: "Your Cart",
-    path: "/cart",
+  res.render('shop/cart', {
+    path: '/cart',
+    pageTitle: 'Your Cart'
   });
 };
 
 exports.getOrders = (req, res, next) => {
-  res.render("shop/orders", {
-    pageTitle: "Your Orders",
-    path: "/orders",
+  res.render('shop/orders', {
+    path: '/orders',
+    pageTitle: 'Your Orders'
   });
 };
 
 exports.getCheckout = (req, res, next) => {
-  res.render("shop/checkout", {
-    pageTitle: "Checkout",
-    path: "/checkout",
+  res.render('shop/checkout', {
+    path: '/checkout',
+    pageTitle: 'Checkout'
   });
 };
--- models/product.js
+++ models/product.js
@@ -1,16 +1,19 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
+
 const p = path.join(
   path.dirname(process.mainModule.filename),
-  "data",
-  "products.json"
+  'data',
+  'products.json'
 );
-const getProductsFromFile = (cb) => {
+
+const getProductsFromFile = cb => {
   fs.readFile(p, (err, fileContent) => {
     if (err) {
-      return cb([]);
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
     }
-    cb(JSON.parse(fileContent));
   });
 };
 
@@ -23,9 +26,9 @@ module.exports = class Product {
   }
 
   save() {
-    getProductsFromFile((products) => {
+    getProductsFromFile(products => {
       products.push(this);
-      fs.writeFile(p, JSON.stringify(products), (err) => {
+      fs.writeFile(p, JSON.stringify(products), err => {
         console.log(err);
       });
     });
